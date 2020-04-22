36c12698ef0bee7d37e1a734201e03aa8deb9b08
Aman Singh <amansinghgts@gmail.com>
2020-04-23 05:11:42 +0530

    Test

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -24,55 +24,55 @@ export class ProfileComponent implements OnInit {
   //   console.log(this.authService.userData.photoURL);
   // }
 
-  updateDp(){
-    // console.log("hello");
-    $('#imageFile').click();
-  }
+  // updateDp(){
+  //   // console.log("hello");
+  //   $('#imageFile').click();
+  // }
   
-  onFileChange(event){
-    const reader = new FileReader();
-    if (event.target.files &&
-      event.target.files.length > 0) {
-      const file = event.target.files[0];
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        $('#preview').attr('src', URL.createObjectURL(event.target.files[0]));
-        // console.log(URL.createObjectURL(file));
-      };
-      this.startUpload(event.target.files);
-  }
-  }
-  startUpload(event: FileList) {
-    // File object
-    const file = event.item(0);
-    // storage path
+  // onFileChange(event){
+  //   const reader = new FileReader();
+  //   if (event.target.files &&
+  //     event.target.files.length > 0) {
+  //     const file = event.target.files[0];
+  //     reader.readAsDataURL(file);
+  //     reader.onload = () => {
+  //       $('#preview').attr('src', URL.createObjectURL(event.target.files[0]));
+  //       // console.log(URL.createObjectURL(file));
+  //     };
+  //     this.startUpload(event.target.files);
+  // }
+  // }
+  // startUpload(event: FileList) {
+  //   // File object
+  //   const file = event.item(0);
+  //   // storage path
 
-    this.imgPath = `test/${new Date().getTime()}_${file.name}`;
-    const fileRef = this.afStorage.ref(this.imgPath);
+  //   this.imgPath = `test/${new Date().getTime()}_${file.name}`;
+  //   const fileRef = this.afStorage.ref(this.imgPath);
     
-    // optional metadata
-    const customMetadata = { app: 'Angular-FireBase-Gallery'};
+  //   // optional metadata
+  //   const customMetadata = { app: 'Angular-FireBase-Gallery'};
    
-    // main task
-    this.task = this.afStorage.upload(this.imgPath, file, { customMetadata });
+  //   // main task
+  //   this.task = this.afStorage.upload(this.imgPath, file, { customMetadata });
 
-    this.afStorage.upload(this.imgPath, file, { customMetadata });
+  //   this.afStorage.upload(this.imgPath, file, { customMetadata });
    
-    // get notified when the download URL is available
-    this.task.snapshotChanges().pipe(
-        finalize(() => {
-          this.downloadURL = fileRef.getDownloadURL();
-          // The above step returns an observable which can be subscribed to fetch the data within it
-          this.downloadURL.subscribe(data => {
-            // storing downloadURL as imageURL
-            this.imageURL = data;    
-            // console.log(this.imageURL);     
+  //   // get notified when the download URL is available
+  //   this.task.snapshotChanges().pipe(
+  //       finalize(() => {
+  //         this.downloadURL = fileRef.getDownloadURL();
+  //         // The above step returns an observable which can be subscribed to fetch the data within it
+  //         this.downloadURL.subscribe(data => {
+  //           // storing downloadURL as imageURL
+  //           this.imageURL = data;    
+  //           console.log(this.imageURL);     
             
-            this.authService.updateURL(this.imageURL);
-          });
+  //           this.authService.updateURL(this.imageURL);
+  //         });
 
-        })
-     ).subscribe();
-  }
+  //       })
+  //    ).subscribe();
+  // }
 
 }
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,13 +10,21 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   
 })
 export class SignUpComponent implements OnInit {
-
+  
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
     
   }
+
   
+  signInCss(){
+    $('#container').removeClass("right-panel-active");
+  }
+
+  signUpCss(){
+    $('#container').addClass("right-panel-active");
+  }
 
   
 
